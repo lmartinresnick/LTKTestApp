73e58fa8fbf68f76a5faab8acb287baec0e4e0aa
lmartinresnick <lmartinresnick@gmail.com>
2022-05-23 20:32:56 -0700

    Initial Commit

---
+++ LTKTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LTKTestApp
+//
+//  Created by Luke Martin-Resnick on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LTKTestApp/LTKTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTKTestAppApp.swift
+//  LTKTestApp
+//
+//  Created by Luke Martin-Resnick on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LTKTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
